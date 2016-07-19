2da0ab70190976a54b28360cf65eeefcb41d37f0
jjackys <jackyho97@hotmail.com>
2016-07-18 21:48:14 -0400

    Added puzzle class

--- src/Solver.java
+++ src/Solver.java
@@ -1,7 +1,24 @@
 
 public class Solver {
-
+	//fits?
+	//recalculate priority
+	//load up numbers from input
+	public void input(Solver s){
+		//assumes puzzle input is correct
+		System.out.println("Please enter");
+	
+	}
+	//output 
+	public Solver(){
+		Puzzle p = new Puzzle();
+		Solver s = new Solver();
+		input(s);
+		//make puzzle
+		//make 9 sub puzzles
+		//make 9 rows and 9 columns
+	}
 	public static void main(String[] args){
 		
 	}
+	
 }
